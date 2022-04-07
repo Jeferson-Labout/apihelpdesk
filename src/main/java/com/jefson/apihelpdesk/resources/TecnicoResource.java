b048fa8ee7a4148d65b2b556a0f95736a243e5a8
jeferson-labout <jefson1989@gmail.com>
2022-04-07 17:09:32 -0300

    Padrão DTO

--- src/main/java/com/jefson/apihelpdesk/resources/TecnicoResource.java
+++ src/main/java/com/jefson/apihelpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jefson.apihelpdesk.domain.Tecnico;
+import com.jefson.apihelpdesk.domain.dtos.TecnicoDTO;
 import com.jefson.apihelpdesk.services.TecnicoService;
 
 
@@ -20,9 +21,9 @@ public class TecnicoResource {
 	private TecnicoService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
 		
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }

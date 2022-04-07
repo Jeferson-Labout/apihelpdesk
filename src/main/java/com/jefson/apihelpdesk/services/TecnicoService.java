551c7fd19ce9870d3c833719c93b144397b1b247
jeferson-labout <jefson1989@gmail.com>
2022-04-07 17:46:02 -0300

    Tratamento de Exceção

--- src/main/java/com/jefson/apihelpdesk/services/TecnicoService.java
+++ src/main/java/com/jefson/apihelpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jefson.apihelpdesk.domain.Tecnico;
 import com.jefson.apihelpdesk.repositories.TecnicoRepository;
+import com.jefson.apihelpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 		
 	}
 

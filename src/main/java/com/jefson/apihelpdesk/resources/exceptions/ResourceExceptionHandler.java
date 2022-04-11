c866780ccc53c580c50d4717f307fc784234306c
jeferson-labout <jefson1989@gmail.com>
2022-04-11 17:03:20 -0300

    CPF E EMAIL DUPLICADO

--- src/main/java/com/jefson/apihelpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/jefson/apihelpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jefson.apihelpdesk.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,4 +22,16 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 
 	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violação de dados!", ex.getMessage(), request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+
+	}
 }
+

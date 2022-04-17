b376175538e7f9697b7fb05b1abb0258e3eebebb
Jeferson-Labout <jefson1989@gmail.com>
2022-04-16 23:55:22 -0300

    JWT

--- src/main/java/com/jefson/apihelpdesk/services/DBService.java
+++ src/main/java/com/jefson/apihelpdesk/services/DBService.java
@@ -3,6 +3,7 @@ package com.jefson.apihelpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jefson.apihelpdesk.domain.Chamado;
@@ -24,12 +25,14 @@ public class DBService {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 
 	public void instaciaDB() {
-		Tecnico tec1 = new Tecnico(null, "Jeferson Labout", "33407705069", "jefson@gmail.com", "123");
+		Tecnico tec1 = new Tecnico(null, "Jeferson Labout", "33407705069", "jefson@gmail.com", encoder.encode("123"));
 		tec1.addPerfil(Perfil.ADMIN);
 
-		Cliente cli1 = new Cliente(null, "Spartakus", "32552279090", "spartakus@gmail.com", "123");
+		Cliente cli1 = new Cliente(null, "Spartakus", "32552279090", "spartakus@gmail.com", encoder.encode("123"));
 
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1,
 				cli1);
